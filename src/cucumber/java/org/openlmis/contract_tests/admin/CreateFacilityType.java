898a5788df8f8ccce83218a340de0c0afbb29e9a
Anna Czyrko <aczyrko@soldevelo.com>
2016-09-05 20:44:15 +0200

    create Facility Type - example

--- src/cucumber/java/org/openlmis/contract_tests/admin/CreateFacilityType.java
+++ src/cucumber/java/org/openlmis/contract_tests/admin/CreateFacilityType.java
@@ -6,10 +6,14 @@ import io.restassured.response.Response;
         import cucumber.api.java.en.Given;
         import io.restassured.http.ContentType;
         import io.restassured.response.Response;
-        import java.io.IOException;
+import javafx.beans.binding.When;
+
+import java.io.IOException;
         import java.util.Properties;
         import static io.restassured.RestAssured.given;
-        import static org.jglue.fluentjson.JsonBuilderFactory.buildObject;
+import static io.restassured.path.json.JsonPath.from;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.jglue.fluentjson.JsonBuilderFactory.buildObject;
         import static org.openlmis.contract_tests.common.LoginStepDefs.ACCESS_TOKEN;
         import static org.openlmis.contract_tests.common.TestVariableReader.baseUrlOfService;
 /**
@@ -17,17 +21,19 @@ import io.restassured.response.Response;
  */
 
     public class CreateFacilityType {
-        private Response createFacilityType;
-        @Given("^I try to create a facilityType with code: (.*)$")
-        public void ITryToCreateAFacilityTypeWith(String code) throws Throwable {
-            createFacilityType = given()
-                    .contentType(ContentType.JSON)
-                    .queryParam("access_token", ACCESS_TOKEN)
-                    .body(buildObject()
-                            .add("code", code)
-                            .getJson().toString())
-                    .when()
-                    .post(baseUrlOfService("requisition") + "facilityTypes");
-        }
+    private Response createFacilityType;
+    private Response getFacilityTypeResponse;
+
+    @Given("^I try to create a facilityType with code: (.*)$")
+    public void ITryToCreateAFacilityTypeWith(String code) throws Throwable {
+        createFacilityType = given()
+                .contentType(ContentType.JSON)
+                .queryParam("access_token", ACCESS_TOKEN)
+                .body(buildObject()
+                        .add("code", code)
+                        .getJson().toString())
+                .when()
+                .post(baseUrlOfService("requisition") + "facilityTypes");
     }
 
+}
